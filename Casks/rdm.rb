9be7faee2c71e0cac1b54d0af16cd3ee0aab76cc
Mike Lewis <mike@mplew.is>
2023-03-21 16:55:28 -0600

    Add Cask definition for RDM
    [This cask was previously removed from the main `homebrew-cask` tap](https://github.com/Homebrew/homebrew-cask/pull/138247) due to the binary being unavailable.
    [I'm now re-hosting that binary](https://github.com/MPLew-is/homebrew-wrappers/releases/tag/rdm-2.2), so this change adds back that Cask definition to this tap, just pointing at the new hosting location.

---
+++ Casks/rdm.rb
@@ -0,0 +1,23 @@
+cask "rdm" do
+  version "2.2"
+  sha256 "4baba78e89fb36ae022d5a234bb968402503ed277af59ae72e8270e9b6bef895"
+
+  url "https://github.com/MPLew-is/homebrew-wrappers/releases/download/rdm-#{version}/RDM-#{version}.pkg",
+      verified: "github.com/MPLew-is/homebrew-wrappers/"
+  name "RDM"
+  desc "Utility to set a Retina display to custom resolutions"
+  homepage "https://github.com/avibrazil/RDM"
+
+  livecheck do
+    skip "Development is abandoned"
+  end
+
+  depends_on macos: ">= :sierra"
+
+  pkg "RDM-#{version}.pkg"
+
+  uninstall quit:    "net.alkalay.RDM",
+            pkgutil: "net.alkalay.RDM"
+
+  zap trash: "~/Library/Preferences/net.alkalay.RDM.plist"
+end
